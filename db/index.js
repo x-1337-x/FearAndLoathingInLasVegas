e2e71ef68a46c5cf6a78e9a2fda0c646c8458368
Joe Bazooka <Joe Bazooka>
2018-03-15 17:31:07 +0200

    HerokuTry09

--- db/index.js
+++ db/index.js
@@ -1,13 +1,13 @@
 const Sequelize = require('sequelize');
 
 const sequelize = new Sequelize('test_db', 'postgres', 'postgres', {
-  host: 'localhost',
+  host: (process.env.DATABASE_URL || 'localhost'),
   dialect: 'postgres',
 });
 
-// if(process.env.DATABASE_URL) sequelize.config.host = process.env.DATABASE_URL;
 
 console.log(process.env.DATABASE_URL)
+console.log(sequelize.config.host)
 const User = sequelize.import('./models/user');
 
 module.exports = {
